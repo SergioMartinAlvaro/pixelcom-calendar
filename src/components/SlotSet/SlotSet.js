6bb80190d9078b8e5b3a9b245745c738bb5d5644
SergioMartinAlvaro <smaral.sergio@gmail.com>
2020-08-16 14:08:23 +0200

    prepared for merge master

--- src/components/SlotSet/SlotSet.js
+++ src/components/SlotSet/SlotSet.js
@@ -120,7 +120,6 @@ class SlotSet extends Component {
         this.setState({
             isLoading: true
         });
-        debugger;
         fetch("http://test.services.pixeltiming.com:4400/booking/availability?date=" + nextProps.date, {
             headers: new Headers({
                 'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZENvbXBhbnkiOjF9.PRKLjlVvM7QHIiBL4gxz5HRREunU8gpWmw78oycSLaU',
